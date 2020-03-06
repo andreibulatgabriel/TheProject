b5d52ac741afe6146f1f73c659d3bc631bc723cf
andrei bulat <bulat_andrei@yahoo.com>
2020-03-07 00:48:03 +0200

    final

--- paginaProdus/paginaProdus.js
+++ paginaProdus/paginaProdus.js
@@ -18,6 +18,21 @@ fetch(`http://localhost:3000/api/watches/${productId}`,
     })
     .then(resp => resp.json())
     .then(response => {
-        console.log(response.price);
+        console.log(response);
+        let productImage = document.getElementById('image');
+        productImage.src = response.image;
+
+        let name = document.getElementById('name');
+        name.innerText = response.name + "   " + response.info.model;
+        let material = document.getElementById('material');
+        material.innerText = "MATERIAL:  " + response.case.material;
+        let diametru = document.getElementById('diametru');
+        diametru.innerText = "DIAMETRU:  " + response.case.diameter;
+        let glass = document.getElementById('glass');
+        glass.innerText = "STICLA:  " + response.case.glass;
+        let strapMaterial = document.getElementById('strap-material');
+        strapMaterial.innerText = "MATERIALUL BRATAREI:  " + response.strap.material;
+        let strapColor = document.getElementById('strap-color');
+        strapColor.innerText = "CULOAREA BRATAREI:  " + response.strap.braceletColor;
     })
     .catch(err => console.log(err));
